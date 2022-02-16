6fda45e76e66f9997b12e15c3854d54275127016
BHARGAB KALITA <52911853+the-wrong-guy@users.noreply.github.com>
2022-02-16 20:15:26 +0530

    Q.2

--- dataStructures.js
+++ dataStructures.js
@@ -1,1 +1,27 @@
-// level {1/2/3}
+// level {2}
+// Q.1
+arr.filter((item)=>item%2)
+// Q.2
+(function(){
+const str = "Hola"
+const vowels = ["a","e","i","o","u"]
+const newStrArr = [...str.toLowerCase()]
+
+const x = newStrArr.map((item)=>{
+const match = vowels.find((e)=> {
+if(e.includes(item)){
+return true
+}else{
+return false
+}
+})
+console.log(match)
+if(match){
+return item.toUpperCase()
+}else{
+item
+};
+
+})
+return x.join('')
+}())
