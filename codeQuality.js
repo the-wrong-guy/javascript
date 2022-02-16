555f5bf4f69403237cab83d04b73a9bcd9e84ef2
BHARGAB KALITA <52911853+the-wrong-guy@users.noreply.github.com>
2022-02-16 20:08:27 +0530

    q1 completed

--- codeQuality.js
+++ codeQuality.js
@@ -1,1 +1,10 @@
-// level {1/2/3}
+// level {2}
+onst getUsers = async ()=> {
+  await Promise.all(getUser(), getProfile(), getPosts())
+  .then((data)=>({
+      user : data[0],
+      profile : data[1],
+      posts : data[2]
+    }))
+  .catch(err=> {throw err})
+  }
